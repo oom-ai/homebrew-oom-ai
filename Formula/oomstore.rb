c17fc4e95e0c37ecf53ce2cc862899ab9f7302cf
Wenxuan <wenxuangm@gmail.com>
2022-02-18 17:07:29 +0800

    oomstore 0.1.1-rc.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/oomstore.rb
+++ Formula/oomstore.rb
@@ -4,10 +4,10 @@
 class Oomstore < Formula
   desc "Toolset for oomstore, a lightweight and fast feature store powered by go."
   homepage "https://github.com/oom-ai/oomstore"
-  url "https://github.com/oom-ai/oomstore/releases/download/v0.1.0/oomstore-v0.1.0-amd64-darwin.tar.gz"
-  sha256 "0dc46edaf09bc7fdffc37de0d12a9d2ac23a737dcb0100c5c90b5f6cc2c873ed"
+  url "https://github.com/oom-ai/oomstore/releases/download/v0.1.1-rc.0/oomstore-v0.1.1-rc.0-amd64-darwin.tar.gz"
+  sha256 "e582cecd876703e5fd7f40aecce2f81c38d27a40a0edfd927e1b14585c954009"
   license "APACHE-2.0"
-  version "0.1.0"
+  version "0.1.1-rc.0"
 
   def install
     bin.install "oomcli"
